8b3c364868eff676397a15785a67bb01e708a400
Jonatan Leal <147429323+jonatan-leal@users.noreply.github.com>
2025-06-24 21:27:02 -0500

    Update LoginTest

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -1,7 +1,7 @@
 package Demo
 
 object Data{
-    val url= "https://parabank.parasoft.com/parabank/services/bank"
-    val username = "john"
-    val password = "demo"
+    val url= "https://thinking-tester-contact-list.herokuapp.com/"
+    val username = "jonleal@mail.com"
+    val password = "jonleal"
 }
