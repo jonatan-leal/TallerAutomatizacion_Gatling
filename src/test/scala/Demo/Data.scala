ab87af7c2a38ecf1497ccd4f440e5c54811d324c
Jonatan Leal <147429323+jonatan-leal@users.noreply.github.com>
2025-06-24 21:29:32 -0500

    Update Data

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -2,6 +2,6 @@ package Demo
 
 object Data{
     val url= "https://thinking-tester-contact-list.herokuapp.com/"
-    val username = "jonleal@mail.com"
+    val email = "jonleal@mail.com"
     val password = "jonleal"
 }
