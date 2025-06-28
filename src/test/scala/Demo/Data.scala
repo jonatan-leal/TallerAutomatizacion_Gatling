5f10ddb45d52d7909c4557acc6895949ac6920f1
Jonatan Leal <147429323+jonatan-leal@users.noreply.github.com>
2025-06-28 13:51:12 -0500

    Minor fixes

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -2,22 +2,22 @@ package Demo
 
 object Data{
     //Base Url
-    val url= "https://thinking-tester-contact-list.herokuapp.com/"
+    val url = "https://thinking-tester-contact-list.herokuapp.com/"
 
     //Login Data
     val email = "jonleal@mail.com"
     val password = "jonleal"
 
     //Contact Data
-    val firstName: "John"
-    val lastName: "Doe",
-    val birthdate: "1970-01-01"
-    val email: "johndoe@mail.com"
-    val phone: "8005555555"
-    val street1: "1 Main St."
-    val street2: "Apartment A"
-    val city: "Anytown"
-    val stateProvince: "KS"
-    val postalCode: "12345"
-    val country: "USA"
+    val firstName = "John"
+    val lastName = "Doe",
+    val birthdate = "1970-01-01"
+    val email = "johndoe@mail.com"
+    val phone = "8005555555"
+    val street1 = "1 Main St."
+    val street2 = "Apartment A"
+    val city = "Anytown"
+    val stateProvince = "KS"
+    val postalCode = "12345"
+    val country = "USA"
 }
